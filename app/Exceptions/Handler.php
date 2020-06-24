2fac6e7f9eb11ba9801300a241d1744a47c5027d
Aleksander Tabor <alexiosan.portable@gmail.com>
2020-06-24 21:35:43 +0200

    Implement createWallet on /wallets @POST endpoint.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -50,6 +51,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json($exception, 404);
+        }
+
+        return response()->json($exception, 500);
     }
 }
--- app/Http/Controllers/TronController.php
+++ app/Http/Controllers/TronController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Wallet;
+use IEXBase\TronAPI\Exception\TronException;
+use IEXBase\TronAPI\Provider\HttpProvider;
+use IEXBase\TronAPI\Tron;
+
 class TronController extends Controller
 {
+    private $api;
+
+    public function __construct()
+    {
+        $fullNode = new HttpProvider('https://api.trongrid.io');
+        $solidityNode = new HttpProvider('https://api.trongrid.io');
+        $eventServer = new HttpProvider('https://api.trongrid.io');
+
+        try {
+            $this->api = new Tron($fullNode, $solidityNode, $eventServer);
+        } catch (TronException $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
     public function createWallet()
     {
+        $wallet = $this->api->createAccount();
+
+        return response()->json(Wallet::create([
+            'address' => $wallet['address'],
+            'secret' => $wallet['privateKey'],
+        ]), 201);
     }
 
     public function getWalletBalance()
