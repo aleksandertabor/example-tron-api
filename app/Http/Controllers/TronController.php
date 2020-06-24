0301aec0e917d57472e1f9971147fc0a6fbe985e
Aleksander Tabor <alexiosan.portable@gmail.com>
2020-06-24 21:20:50 +0200

    Create TronController for creating wallets and getting balance from them.

---
+++ app/Http/Controllers/TronController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TronController extends Controller
+{
+    public function createWallet()
+    {
+    }
+
+    public function getWalletBalance()
+    {
+    }
+}
